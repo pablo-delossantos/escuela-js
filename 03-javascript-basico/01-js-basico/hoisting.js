389c1ce99d8e4f618ef615a2b7efe0c11cddd274
Pablo de los Santos <estudiodedalo@gmail.com>
2021-02-17 02:49:47 -0300

    Hoisting + Archivos de ejemplo de todo el curso de JS básico

---
+++ 03-javascript-basico/01-js-basico/hoisting.js
@@ -0,0 +1,11 @@
+console.log(miNombre); //Funcion que nos da el navegador para que podamos ver en la consola lo que está pasado.
+
+var miNombre = "Pablo";
+
+hey();
+
+function hey() {
+  console.log("Hola " + miNombre)
+}
+
+//Por buenas prácticas todas las funciones que vamos a ocupar en el código, se declaran antes del código. 
