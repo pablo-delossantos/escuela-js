fd5c94c8107518cd48abf084f5285d0ffa853f39
Pablo de los Santos <estudiodedalo@gmail.com>
2021-02-17 00:47:14 -0300

    Carpeta inicial del curso de JS básico y primer archivo con valores.js

---
+++ 03-javascript-basico/01-js-basico/valores.js
@@ -0,0 +1,17 @@
+// VALORES PRIMITIVOS
+40
+
+//string: cadenas de texto
+"Pablo de los Santos" 
+
+//Booleanos
+true 
+false 
+
+//Valores vacios (empty values): Son valores reservados para un tipo de valor. Cuando queda un valor faltante, podemos ocupar uno de estos valores para reservar espacio en memoria.
+null
+undefined
+
+// VALORES NO PRIMITIVOS O VALORES TIPO OBJETOS
+[1,2,3] //Valores primitivos
+{ nombre: "Pablo" } //Objeto
