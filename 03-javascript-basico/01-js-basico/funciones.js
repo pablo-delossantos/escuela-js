5c95e0c4ffb505f98af30ae363ad78a0245c59b0
Pablo de los Santos <estudiodedalo@gmail.com>
2021-02-17 01:39:11 -0300

    Funciones

---
+++ 03-javascript-basico/01-js-basico/funciones.js
@@ -0,0 +1,15 @@
+// Funciones Declarativas
+
+function miFuncion() {
+  return 3;
+}
+
+miFuncion();
+
+// Funciones de Expresión (o anónimas)
+
+var miFuncion = function(a,b){
+  return a + b;
+}
+
+miFuncion();
