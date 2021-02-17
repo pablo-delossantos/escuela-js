017c250fefed25ddb204ac8ebbf84867b2e9ebcf
Pablo de los Santos <estudiodedalo@gmail.com>
2021-02-17 01:10:11 -0300

    Variables en JS

---
+++ 03-javascript-basico/01-js-basico/variables.js
@@ -0,0 +1,20 @@
+// Variable. Guarda algun lugar en memoria para guardar un valor.
+var nombre = "Pablo";
+
+// Hay dos estados de una variable
+// declarar
+var edad;
+
+edad
+
+// inicializar
+edad = 30; // El operador "=" sirve para inicializar una variable.
+
+// Esto es una Array
+var elementos = ["Computadora", "Celular"];
+
+// Forma de generar un objeto guardado en una variable.
+var persona = {
+  nombre: "Pablo",
+  edad: 42
+}
