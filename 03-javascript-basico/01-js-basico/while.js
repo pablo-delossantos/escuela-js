d3d50010260fc1d29e78e5922be40fe237a03d2e
Pablo de los Santos <estudiodedalo@gmail.com>
2021-02-20 12:40:07 -0300

    Archo

---
+++ 03-javascript-basico/01-js-basico/while.js
@@ -0,0 +1,10 @@
+var estudiantes = ["María", "Pablo", "Rosa", "Daniel"];
+
+function saludarEstudiantes(estudiante){
+    console.log(`Hola, ${estudiante}`);  
+}
+
+while(estudiantes.length > 0){
+    var estudiante = estudiantes.shift();
+    saludarEstudiantes(estudiante);
+}
