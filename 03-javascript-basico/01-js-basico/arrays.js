732a7d2f077d0d4342ca42830e8c3d4f2787151a
Pablo de los Santos <estudiodedalo@gmail.com>
2021-02-19 23:33:42 -0300

    Método Array

---
+++ 03-javascript-basico/01-js-basico/arrays.js
@@ -0,0 +1,13 @@
+//Tipo objeto. Es una lista de datos.
+
+var frutas = ["Manzana", "Banana", "Cereza", "Frutilla"];
+
+var masFrutas = frutas.push("Uvas"); //.push / Método para agregar un elemento al final de nuestro array.
+
+console.log(frutas[3]);
+
+var masFrutas = frutas.pop("Uvas"); //.pop / Quita el último elemento del array.
+
+var nuevaLongitud = frutas.unshift("Sandía"); //.unshift / Agrega un elemnto al inicio del Array.
+
+var borrarFruta = frutas.shift("Manzana"); //.shift / Borra un elemento al inicio del array.
