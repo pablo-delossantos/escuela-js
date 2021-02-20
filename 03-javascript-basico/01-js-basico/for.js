37a9193e75990181f6752b33edb8894368d1cefd
Pablo de los Santos <estudiodedalo@gmail.com>
2021-02-20 12:31:28 -0300

    ejemplo de for of en el archivo for.js

--- 03-javascript-basico/01-js-basico/for.js
+++ 03-javascript-basico/01-js-basico/for.js
@@ -4,14 +4,12 @@ function saludarEstudiantes(estudiante){
     console.log(`Hola, ${estudiante}`);  
 }
 
-for(var estudiante of estudiantes){
-    saludarEstudiantes(estudiante);
-}
-
-//estudiante (en singular) es el index del grupo (estuduantes)
-
 for(var i = 0; i < estudiantes.length; i++){
   saludarEstudiantes(estudiantes[i]);
 }
-
 //Casi siempre el for ocupa una variable i : for(var i = 0;)...
+
+for(var estudiante of estudiantes){
+  saludarEstudiantes(estudiante);
+}
+//for of : estudiante (en singular) es el index del grupo (estuduantes)
