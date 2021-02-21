3364becdd0446af84e6d32ebcd74681762f70a0e
Pablo de los Santos <estudiodedalo@gmail.com>
2021-02-21 00:41:27 -0300

    Clase 1 variables

---
+++ 04-fundamentos-js/clase01.js
@@ -0,0 +1,10 @@
+//Declaramos la variable y le asignamos el valor al mismo tiempo. La , (coma) hace que se declaren las dos variables a la vez
+var nombre = 'Pablo', apellido = 'de los Santos'
+var edad = 42
+
+edad = '42 años' //Debilmente tipado: acepta distintos tipos de variables.
+
+console.log('Hola ' + nombre + ' ' + apellido) //Signo + concatena
+console.log('Tengo ' + edad)
+
+var peso = 75
