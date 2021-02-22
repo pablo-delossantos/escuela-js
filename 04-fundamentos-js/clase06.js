9187d0ab5cbcf555dff4e541e26e57c66e79545d
Pablo de los Santos <estudiodedalo@gmail.com>
2021-02-22 00:06:22 -0300

    Clase 6 Objetos

--- 04-fundamentos-js/clase06.js
+++ 04-fundamentos-js/clase06.js
@@ -1,8 +1,22 @@
-var nombre = 'Pablo'
-
-function imprimirNombreEnMayusculas(nombre) {
-    nombre = nombre.toUpperCase()
-    console.log(nombre)
+var pablo = {
+    nombre: 'Pablo',
+    apellido: 'de los Santos',
+    edad: 42
 }
 
-imprimirNombreEnMayusculas(nombre)
+var dario = {
+    nombre: 'Juan Manuel',
+    apellido: 'Villulla',
+    edad: 38
+}
+
+//Las llaves delimitan un 'objeto' que va a tener atributos. Estos son 'claves' (key) y 'valores' (value)
+
+function imprimirNombreEnMayusculas({ nombre }) {
+    console.log(nombre.toUpperCase())
+}
+
+imprimirNombreEnMayusculas(pablo)
+imprimirNombreEnMayusculas(dario)
+//imprimirNombreEnMayusculas({ nombre: 'Pepito' }) Error. Vacío
+//imprimirNombreEnMayusculas({ apellido: 'Gomez'}) Error. No tiene atributo nombre
