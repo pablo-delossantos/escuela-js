abfed79c431a29c958c6b91d730c8a684eb805d1
Pablo de los Santos <estudiodedalo@gmail.com>
2021-02-22 22:20:02 -0300

    Clase 9 Comparaciones en JS

---
+++ 04-fundamentos-js/clase09.js
@@ -0,0 +1,15 @@
+//El signo = sirve para asignar un valor a una variable
+var x = 4, y = '4'
+
+// Si usamos == nos sirve para comparar
+
+//Si usamos === nos sirve para comparar exactamente igual. Siempre que podamos usemos el ===
+
+var pablo = {
+    nombre: 'Pablo'
+}
+
+var otraPersona = {
+    nombre: 'Pablo'
+} //Se crea un nuevo objeto que ocupa otro lugar en memoria ram
+
