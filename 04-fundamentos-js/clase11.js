5b50a8ecf4172c6cc5ca9408ff398be79e4080a8
Pablo de los Santos <estudiodedalo@gmail.com>
2021-02-23 00:00:04 -0300

    Clase 11 Funciones que retornan valores

---
+++ 04-fundamentos-js/clase11.js
@@ -0,0 +1,61 @@
+var pablo = {
+    nombre: 'Pablo',
+    apellido: 'de los Santos',
+    edad: 42,
+    disenador: true,
+    cocinero: true,
+    dj: false,
+    cantante: false,
+    guitarrista: false,
+    drone: false
+}
+
+var juan = {
+    nombre: 'Juan',
+    apellido: 'Gomez',
+    edad: 13
+}
+
+function imprimirProfesiones(persona) {
+    console.log(`${persona.nombre} es:`)
+
+    if (persona.disenador) {
+        console.log('Diseñador') //Solo se tiene que hacer si es true
+    } else {
+        console.log('No es diseñador')
+    }
+
+    if (persona.cocinero) {
+        console.log('Cocinero')
+    }
+
+    if (persona.dj) {
+        console.log('Dj')
+    }
+
+    if (persona.cantante) {
+        console.log('Cantante')
+    }
+
+    if (persona.guitarrista) {
+        console.log('Guitarrista')
+    }
+
+    if (persona.drone) {
+        console.log('Piloto de drone')
+    }
+}
+
+const MAYORIA_DE_EDAD = 18 //Es una constante
+
+function esMayorDeEdad(persona){
+    return persona.edad >= MAYORIA_DE_EDAD
+} //Si se invoca, devuelvo true o false
+
+function imprimirSiEsMayorDeEdad(persona) {
+    if (esMayorDeEdad(persona)) {
+        console.log(`${persona.nombre} es mayor de edad`)
+    } else {
+        console.log(`${persona.nombre} es menor de edad`)
+    }
+}
