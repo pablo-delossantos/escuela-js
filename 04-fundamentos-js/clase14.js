594c6ec6c59d5a0c2d8861d899297879098ac031
Pablo de los Santos <estudiodedalo@gmail.com>
2021-02-25 23:51:04 -0300

    Repaso clase 13 ciclo for + Clase 14 ciclo While

---
+++ 04-fundamentos-js/clase14.js
@@ -0,0 +1,36 @@
+var pablo = {
+    nombre: 'Pablo',
+    apellido: 'de los Santos',
+    edad: 42,
+    peso: 75
+}
+
+console.log(`Al inicio del año ${pablo.nombre} pesa ${pablo.peso} kg.`)
+
+// function aumentarDepeso (persona) {
+//     return persona.peso += 0.3
+// }
+
+const INCREMENTO_PESO = 0.3
+const DIAS_DEL_ANO = 365
+
+const aumentarDePeso = persona => persona.peso += INCREMENTO_PESO
+const adelgazar = persona => persona.peso -= INCREMENTO_PESO
+const comeMucho = () => Math.random() < 0.3
+const realizaDeporte = () => Math.random() < 0.4
+
+const META = pablo.peso - 3
+var dias = 0
+
+//El ciclo while se va a repetir mientras la condicion sea verdadera y se cumpla.
+while (pablo.peso > META) {
+    if (comeMucho()) {
+        aumentarDePeso(pablo)
+    }    
+    if (realizaDeporte()) {
+        adelgazar(pablo)
+    }
+    dias += 1
+}
+
+console.log(`Pasaron ${dias} días hasta que ${pablo.nombre} adelgazó 3kg.`)
